813e5748f47c0914cce1cc227954296553e7041b
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-11-22 17:47:26 -0300

    [ADD] epa_hr_attendances_overtime_custom

---
+++ epa_hr_attendances_overtime_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Epa Hr Attendances Overtime Custom",
+    "summary": """
+        EPA HR Attendances Overtime Custom""",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/epa-addons",
+    "depends": ["hr_attendances_overtime"],
+    "data": [
+        "views/hr_attendance_sheet.xml",
+    ],
+    "demo": [],
+}
