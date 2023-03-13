554c3c5492c6dbd33a18c1f9557e056e1b3aa09d
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-03-13 18:21:13 -0300

    [RFC] epa_hr_attendances_overtime_custom: refactor leave status and others

--- epa_hr_attendances_overtime_custom/models/hr_attendance_sheet.py
+++ epa_hr_attendances_overtime_custom/models/hr_attendance_sheet.py
@@ -1,8 +1,9 @@
 # Copyright 2022 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from datetime import timedelta
+from datetime import datetime, time, timedelta
 
+import pytz
 from pytz import timezone
 
 from odoo import api, fields, models
@@ -60,30 +61,26 @@ class HrAttendanceSheet(models.Model):
                     vals.update({"status": "weekend"})
 
                 if vals["psignin"] and vals["psignout"]:
-                    if vals["asignin"] == 0.0 and vals["asignout"] == 0.0:
-                        resource_id = self.employee_id.resource_id
-                        leave = (
-                            self.employee_id.resource_calendar_id.leave_ids.filtered(
-                                lambda l: (
-                                    not l.resource_id or l.resource_id == resource_id
-                                )
-                                and timezone("UTC")
-                                .localize(l.date_from)
-                                .astimezone(timezone(resource_id.tz))
-                                .date()
-                                <= date
-                                and timezone("UTC")
-                                .localize(l.date_to)
-                                .astimezone(timezone(resource_id.tz))
-                                .date()
-                                >= date
-                            )
-                        )
+                    # if vals["asignin"] == 0.0 and vals["asignout"] == 0.0:
+                    resource_id = self.employee_id.resource_id
+                    leave = self.employee_id.resource_calendar_id.leave_ids.filtered(
+                        lambda l: (not l.resource_id or l.resource_id == resource_id)
+                        and timezone("UTC")
+                        .localize(l.date_from)
+                        .astimezone(timezone(resource_id.tz))
+                        .date()
+                        <= date
+                        and timezone("UTC")
+                        .localize(l.date_to)
+                        .astimezone(timezone(resource_id.tz))
+                        .date()
+                        >= date
+                    )
 
-                        if leave:
-                            vals.update({"status": "leave"})
-                        if not leave:
-                            vals.update({"status": "absence"})
+                    if leave:
+                        vals.update({"status": "leave"})
+                    if not leave and vals["asignin"] == 0.0 and vals["asignout"] == 0.0:
+                        vals.update({"status": "absence"})
 
                 if vals["asignin"] > vals["psignin"] and vals["status"] == "weekday":
                     late = vals["asignin"] - vals["psignin"]
@@ -135,8 +132,38 @@ class HrAttendanceSheet(models.Model):
                             if not line.date_to == date or line.date_from == date:
                                 vals = {}
 
-                if vals["status"] in ["weekday", "absence"]:
-                    vals.update({"total_planned_attendance": avg_hours})
+                # Calcula corretamente o tempo planejado
+                tz = self.employee_id.resource_id.calendar_id.tz
+                if vals["status"] in ["weekday", "absence", "leave"]:
+                    vals.update(
+                        {
+                            "total_planned_attendance": self.employee_id.with_context(
+                                exclude_public_holidays=True,
+                                employee_id=self.employee_id.id,
+                            ).get_work_days_data(
+                                datetime.combine(
+                                    date, time(0, 0, 0, 0, tzinfo=pytz.timezone(tz))
+                                ),
+                                datetime.combine(
+                                    date,
+                                    time(23, 59, 59, 99999, tzinfo=pytz.timezone(tz)),
+                                ),
+                                # Pass this domain for excluding leaves whose type is
+                                # included in theoretical hours
+                                domain=[
+                                    "|",
+                                    ("holiday_id", "=", False),
+                                    (
+                                        "holiday_id.holiday_status_id.include_in_theoretical",
+                                        "=",
+                                        False,
+                                    ),
+                                ],
+                            )[
+                                "hours"
+                            ]
+                        }
+                    )
 
                 if flage and vals:
                     self.env["hr.attendance.sheet.line"].create(vals)
@@ -155,6 +182,9 @@ class HrAttendanceSheet(models.Model):
                 )
             rec.total_attendance = total_attendance
             rec.total_planned_attendance = total_planned_attendance
+            total_absence = rec.total_planned_attendance - rec.total_attendance
+            if total_absence >= 0:
+                rec.total_absense = total_absence
 
     @api.multi
     def print_document(self):
