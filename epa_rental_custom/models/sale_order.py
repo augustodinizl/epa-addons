cbe243b13cf269d32c97e0eb9955147653fb3203
kaynnan <kaynnan.lemes@escodoo.com.br>
2023-07-27 15:37:22 -0300

    [IMP] epa_rental_custom: field responsible_technical

--- epa_rental_custom/models/sale_order.py
+++ epa_rental_custom/models/sale_order.py
@@ -1,8 +1,10 @@
 # Copyright 2023 - TODAY, Marcel Savegnago <marcel.savegnago@escodoo.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    responsible_technical = fields.Many2one("res.users", string="Responsible Technical")
